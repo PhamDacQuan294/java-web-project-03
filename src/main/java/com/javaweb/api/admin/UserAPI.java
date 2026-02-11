f57186bbfa196765369a5137f9cafc7a1d595a3f
quan <quan93909@gmail.com>
2026-02-11 09:54:07 +0700

    Khoi tao

---
+++ src/main/java/com/javaweb/api/admin/UserAPI.java
@@ -0,0 +1,12 @@
+package com.javaweb.api.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserAPI {
+  @GetMapping("/test")
+  public String testAPI() {
+    return "success";
+  }
+}
